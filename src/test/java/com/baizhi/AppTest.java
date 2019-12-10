b0859e44ea96166a999418d6c27721b017d23571
chunchun946444 <58738135+chunchun946444@users.noreply.github.com>
2019-12-11 00:52:16 +0800

    添加两行数据
    同上

--- src/test/java/com/baizhi/AppTest.java
+++ src/test/java/com/baizhi/AppTest.java
@@ -14,7 +14,9 @@ public class AppTest
      */
     @Test
     public void shouldAnswerWithTrue()
-    {
+    {   
+        
+        System.out.println("github上添加的内容！");
         assertTrue( true );
     }
 }
